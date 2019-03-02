0e13a71ce73e8086eda83d1590d5c140849e5b35
subhashreehazra <42761654+subhashreehazra@users.noreply.github.com>
2019-03-02 15:09:54 +0530

    Add files via upload

---
+++ log.py
@@ -0,0 +1,23 @@
+import win32evtlog 
+server = 'localhost' 
+logtype = 'System' 
+hand = win32evtlog.OpenEventLog(server,logtype)
+flags = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
+total = win32evtlog.GetNumberOfEventLogRecords(hand)
+current =0
+while (current<20):
+    events = win32evtlog.ReadEventLog(hand, flags,0)
+    if events:
+        for event in events:
+            print ('Event Category:', event.EventCategory)
+            print ('Time Generated:', event.TimeGenerated)
+            print ('Source Name:', event.SourceName)
+            print ('Event ID:', event.EventID)
+            print ('Event Type:', event.EventType)
+            data = event.StringInserts
+            if data:
+                print ('Event Data:')
+                for msg in data:
+                    print (msg)
+            print
+    current+=1
---
+++ net.py
@@ -0,0 +1,28 @@
+
+
+
+import time
+import psutil
+import os
+import json
+
+
+
+def stream_host_stats():
+    while True:
+        net = psutil.net_io_counters(pernic=True)
+        time.sleep(1)
+        net1 = psutil.net_io_counters(pernic=True)
+        net_stat_download = {}
+        net_stat_upload = {}
+        print("hello")
+        for k, v in net.items():
+            for k1, v1 in net1.items():
+                if k1 == k:
+                    net_stat_download[k] = (v1.bytes_recv - v.bytes_recv) / 1000.
+                    net_stat_upload[k] = (v1.bytes_sent - v.bytes_sent) / 1000.
+                    print(json.dumps(net_stat_download))
+                    print(json.dumps(net_stat_upload))
+
+stream_host_stats()                
+        
